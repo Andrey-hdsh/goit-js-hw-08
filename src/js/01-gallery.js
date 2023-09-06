dbdd804e99cfda593b8810b917f553f0674d7f72
Andrey-hdsh <hdsh138720@gmail.com>
2023-09-06 13:14:28 +0300

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainerEl = document.querySelector('.gallery');
+
+renderGallery(galleryItems);
+
+function renderGallery(array) {
+    const galleryMarkup = array.map(items =>
+        `<li class="gallery__item">
+    <a class="gallery__link" href="${items.original}">
+    <img
+      class="gallery__image"
+      src="${items.preview}"
+      alt="${items.description}"/>
+    </a>
+        </li>`).join('');
+    galleryContainerEl.insertAdjacentHTML('beforeend' , galleryMarkup)
+};
+
+ const lightbox = new SimpleLightbox('.gallery a', {
+        
+        captionsData: "alt",
+        captionDelay: 250,
+        animationSpeed: 300
+    });
+
