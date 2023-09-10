68ab151a2d88b075dc6a016559ad59dd406b9dda
Andrey-hdsh <hdsh138720@gmail.com>
2023-09-10 14:16:06 +0300

    commit_1.1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+// ***************************************************
+const iframe = document.querySelector('iframe');
+// console.log(iframe);
+const player = new Player(iframe);
+const VIDEO_TIME_KEY = 'videoplayer-current-time';
+
+function localStorageValue(event) {
+    localStorage.setItem('VIDEO_TIME_KEY', event.seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('VIDEO_TIME_KEY')).then(function (seconds) {
+}).catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
+
+player.on("timeupdate", throttle(localStorageValue, 1000));
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const modalForm = document.querySelector('form');
+const FORM_DATA_KEY = 'feedback-form-state';
+
+modalForm.addEventListener('submit', onFormSubmit);
+
+
+
+const trottledSave = throttle(savingFormData , 500);
+function savingFormData () {
+    const formDataElements = {
+        email: modalForm.elements.email.value,
+        message: modalForm.elements.message.value
+    };
+    localStorage.setItem('FORM_DATA_KEY', JSON.stringify(formDataElements));
+    // console.log(formDataElements);
+};
+
+modalForm.addEventListener('input', trottledSave);
+
+const localInfo = localStorage.getItem('FORM_DATA_KEY');
+if (localInfo) {
+    const parseLocaleInfo = JSON.parse(localInfo);
+    modalForm.elements.email.value = parseLocaleInfo.email || '';
+    modalForm.elements.message.value = parseLocaleInfo.message || '';
+} else {
+    modalForm.reset();
+};
+
+
+function onFormSubmit(event) {
+event.preventDefault();
+// console.log(event);
+if (modalForm.elements.email.value !== '' || modalForm.elements.message.value !== '') {
+    console.log({ email: modalForm.elements.email.value, message: modalForm.elements.message.value });
+    modalForm.reset();
+    }
+};
